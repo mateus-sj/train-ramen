8efdca7f9b4f306bdbe396695ed25a67ae0e35d9
mateus-sj <mateussj12@gmail.com>
2023-10-02 11:51:16 -0300

    Added wandb integration, corrected variable names on train_roberta_ramen.sh

--- ramen_pytorch.py
+++ ramen_pytorch.py
@@ -3,14 +3,81 @@
 import os
 import math
 import torch
+import numpy as np
+import time
+import wandb
+import argparse
 from torch.cuda.amp import autocast, GradScaler  # Native AMP
 from models import RobertaMLM, RobertaAdaptor, BertMLM, BertAdaptor
 from ramen.code.src.data.loader import DataIterator
-import numpy as np
 from ramen.code.src.logger import init_logger
-from ramen.code.opt import get_parser
-import time
 from ramen.code.src.optim import AdamInverseSquareRootWithWarmup
+
+
+def get_parser():
+    parser = argparse.ArgumentParser(description="RAMEN-LM")
+    parser.add_argument("--exp_path", type=str, default="../experiments",
+                        help="path to store experiments dir model")
+    parser.add_argument("--src_pretrained_path", type=str, default="",
+                        help="path to [bert|xlnet] pretrained dir model")
+    parser.add_argument("--tgt_pretrained_path", type=str, default="",
+                        help="path to [bert|xlnet] pretrained dir model")
+    parser.add_argument("--pretrained_path", type=str, default="",
+                        help="path to [bert|roberta|xlnet] pretrained dir model")
+    parser.add_argument("--xnli_model", type=str, default="",
+                        help="path to trained xnli model")
+
+    parser.add_argument("--ud_model", type=str, default="",
+                        help="path to trained parser")
+    # masked LM params
+    parser.add_argument("--bptt", type=int, default=256,
+                        help="number of tokens in a sentence.")
+    parser.add_argument("--word_pred", type=float, default=0.15,
+                        help="% of masked words")
+    # batch parameters
+    parser.add_argument("--batch_size", type=int, default=32,
+                        help="number of sentences per batch")
+
+    parser.add_argument("--fp16", action='store_true',
+                        help="use float16 for training")
+    parser.add_argument("--opt_level", type=str, default='O2',
+                        choices=['O1', 'O2'],
+                        help='optimization level')
+    # data
+    parser.add_argument("--data_path", type=str, default="../data/bert/binary",
+                        help="Data path")
+    parser.add_argument("--src_lang", type=str, default="",
+                        help="source languages")
+    parser.add_argument("--tgt_lang", type=str, default="",
+                        help="target languages")
+    parser.add_argument("--max_epoch", type=int, default=100,
+                        help="max number of training epoch")
+    parser.add_argument("--epoch_size", type=int, default=50000,
+                        help="number of updates per epoch")
+    parser.add_argument("--lr", type=float, default=0.000005,
+                        help="learning rate of Adam")
+    parser.add_argument("--optim", type=str, default='adam',
+                        help="optimizer.")
+    parser.add_argument("--grad_acc_steps", type=int, default=1,
+                        help="gradient accumulation steps")
+    parser.add_argument("--debug_train", action='store_true',
+                        help='fast debugging model')
+    parser.add_argument("--tokenizer", choices=["bert", "roberta"],
+                        default="bert", type=str,
+                        help="tokenizer, needed for special tokens")
+    parser.add_argument("--src_model", choices=["bert", "roberta"],
+                        default="bert", type=str,
+                        help="source pre-trained model")
+    parser.add_argument("--log_freq", type=int, default=50,
+                        help="Number of steps for logging")
+    parser.add_argument("--log_wandb", action='store_true',
+                        help="Log using wandb")
+    parser.add_argument("--project_name", type=str,
+                        help="Wandb project name")
+    parser.add_argument("--run_name", type=str,
+                        help="Wandb run name")
+
+    return parser
 
 
 # util for printing time
@@ -50,7 +117,7 @@ def mask_input(x, word_pred, mask_index):
     # masked words to predict
     y_idx = np.random.choice(bsize * slen, npred, replace=False)
     # keep some identity words
-    i_idx = np.random.choice(npred, int(0.10*npred), replace=False)
+    i_idx = np.random.choice(npred, int(0.10 * npred), replace=False)
     mask = torch.zeros(slen * bsize, dtype=torch.long)
     mask[y_idx] = 1
     # identity (i.e copy)
@@ -64,7 +131,7 @@ def mask_input(x, word_pred, mask_index):
     # process x
     mask_ = mask_.view(bsize, slen)
     mask_[:, 0] = 0
-    x.masked_fill_(mask_ == 1, mask_index)   # mask_index
+    x.masked_fill_(mask_ == 1, mask_index)  # mask_index
 
     return x.cuda(), y.cuda()
 
@@ -72,7 +139,8 @@ def mask_input(x, word_pred, mask_index):
 def main():
     parser = get_parser()
     params = parser.parse_args()
-
+    if params.log_wandb:
+        wandb.init(project=params.project_name, config=vars(params), name=params.run_name)
     if not os.path.exists(params.exp_path):
         os.makedirs(params.exp_path)
 
@@ -89,6 +157,8 @@ def main():
     tgt_model = pretrained_model.from_pretrained(params.tgt_pretrained_path)
     model = adaptor(src_model, tgt_model)
     model = model.cuda()
+    if params.log_wandb:
+        wandb.watch(model, log_freq=params.log_freq)
 
     optimizer = AdamInverseSquareRootWithWarmup(model.parameters(), lr=params.lr, warmup_updates=4000)
 
@@ -105,6 +175,7 @@ def main():
     def evaluate(lang, loader):
         model.eval()
         losses = []
+        print("Evaluating model")
         for x in loader:
             x, y = mask_input(x.squeeze(0), params.word_pred, mask_index)
             with autocast():  # Native AMP
@@ -138,11 +209,12 @@ def main():
             loss_en = step(src_lg, batch_en, False)
             loss_fr = step(tgt_lg, batch_fr, n_iter % params.grad_acc_steps == 0)
 
-            if n_iter % 50 == 0:
+            if n_iter % params.log_freq == 0:
                 time_taken = time.time() - start_time
                 hours, mins, secs = _to_hours_mins_secs(time_taken)
-
                 cur_lr = optimizer.get_lr()
+                if params.log_wandb:
+                    wandb.log({"loss_source": loss_en, "loss_target": loss_fr, "learning_rate": cur_lr}, step=n_iter)
                 logger.info(
                     f" Iter {n_iter:>7} - MLM-{src_lg} {loss_en:.4f} -"
                     f" MLM-{tgt_lg} {loss_fr:.4f} - lr {cur_lr:.7f}"
@@ -150,9 +222,13 @@ def main():
                 )
 
             if n_iter % params.epoch_size == 0:
+                print(f" Evaluation and CKP Epoch {n_epoch} - Step: {n_iter}")
                 n_epoch += 1
                 valid_src_nll = evaluate(src_lg, valid_loader_en)
                 valid_tgt_nll = evaluate(tgt_lg, valid_loader_fr)
+                if params.log_wandb:
+                    wandb.log({"source_validation_loss": valid_src_nll, "target_validation_loss": valid_tgt_nll},
+                              step=n_iter)
                 logger.info(
                     f" Validation - Iter {n_iter} |"
                     f" MLM-{src_lg} {valid_src_nll:.4f} MLM-{tgt_lg} {valid_tgt_nll:.4f}"
@@ -178,6 +254,7 @@ def main():
 
                 if n_epoch == params.max_epoch:
                     exit()
+
 
 if __name__ == '__main__':
     main()
--- train_roberta_ramen.sh
+++ train_roberta_ramen.sh
@@ -9,9 +9,14 @@ VOCAB_DIR=/work/vocabs
 TGT_INIT=/work/roberta_target_init
 PROB_PATH=/work/probs/probs.mono.pt-jur.pth
 
-# HYPERPARAMETERS
-N_UPDATES=20000
+# HYPERPARAMETERS AND CONFIG
+EPOCH_SIZE=20000
 MAX_EPOCH=6
+LOG_FREQ=50
+BATCH_SIZE=4
+BPTT=256
+GRAD_ACC_STEPS=8
+LEARNING_RATE=0.0001
 
 # VOLUMES
 # INPUT DATA DIR (.pth files) AND PRETRAINED MODEL
@@ -20,5 +25,11 @@ PRETRAINED_DIR=/work/models/roberta_pretrained
 # SAVE PATH
 EXP_PATH=/work/models/ramen_jur_roberta
 
+#WANDB
+export WANDB_API_KEY="wandb_api_key"
+PROJECT_NAME="test_ramen"
+RUN_NAME="test_ramen_v8"
+
+
 python init_weight_alignment.py --src_vocab "${PRETRAINED_DIR}/vocab.json" --src_model ${PRETRAINED_DIR} --prob ${PROB_PATH} --tgt_model "$TGT_INIT/pytorch_model.bin" --tgt_vocab "$VOCAB_DIR/jur_vocab.txt" --src_merge "$PRETRAINED_DIR/merges.txt"
-python ramen_pytorch.py --lr 0.0001 --tgt_lang jur --src_lang pt --batch_size 4 --bptt 256 --src_pretrained_path ${PRETRAINED_DIR} --tgt_pretrained_path ${TGT_INIT} --data_path ${BIN_PATH} --epoch_size ${N_UPDATES} --max_epoch ${MAX_EPOCH} --fp16 --exp_path ${EXP_PATH} --grad_acc_steps 8 --src_model "roberta" --tokenizer "roberta"
+python ramen_pytorch.py --lr ${LEARNING_RATE} --tgt_lang jur --src_lang pt --batch_size ${BATCH_SIZE} --bptt ${BPTT} --src_pretrained_path ${PRETRAINED_DIR} --tgt_pretrained_path ${TGT_INIT} --data_path ${BIN_PATH} --epoch_size ${EPOCH_SIZE} --max_epoch ${MAX_EPOCH} --fp16 --exp_path ${EXP_PATH} --grad_acc_steps ${GRAD_ACC_STEPS} --src_model "roberta" --tokenizer "roberta"  --log_wandb --project_name ${PROJECT_NAME} --run_name ${RUN_NAME} --log_freq ${LOG_FREQ}
